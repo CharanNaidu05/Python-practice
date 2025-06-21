fb3efad381b525cac0116fdfcbfbef7b350c28c2
Charan Naidu <ncharannaidu987@gmail.com>
2025-06-21 16:12:04 +0530

    Add files via upload

---
+++ formate specifier.py
@@ -0,0 +1,16 @@
+price1 = 3000.14159
+price2 = 1200.34783
+price3 = -9870.65 
+
+#print(f"price 1 is {price1:.1f}")
+#print(f"price 1 is {price2:.2f}")
+#print(f"price 1 is {price3:.3f}")
+
+##print(f"price 1 is {price1:+}")
+#print(f"price 1 is {price2:+}")
+#print(f"price 1 is {price3:+}")
+
+###Thousand sperator
+print(f"price 1 is {price1:,.2f}")
+print(f"price 1 is {price2:,.2f}")
+print(f"price 1 is {price3:,.2f}")
---
+++ string index.py
@@ -0,0 +1,9 @@
+credit_number = "1234-5678-9012-3456"
+#last_digits = credit_number[-4:]
+#print (f"xxxx-xxxx-xxxx-{last_digits}")
+
+##to print reverse string -1
+credit_number = credit_number[::-1]
+print(credit_number)
+
+###print(credit_number[5:9])
